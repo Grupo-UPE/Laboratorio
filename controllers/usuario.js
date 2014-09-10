97adf000bc7b0802681a470d66bfdb948796f3d2
Ferticidio <ferticidio@gmail.com>
2014-09-10 19:34:21 -0300

    Usuario desarrollado, password encriptada, mejor login

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -1,42 +1,62 @@
+var Usuario = require('../models/usuario');
+
 var mongoose        = require('mongoose')
   , db_lnk          = 'mongodb://localhost/test'
   , db              = mongoose.createConnection(db_lnk)
 
+
   var usuario_schema = require('../models/usuario')
   , Usuario = db.model('Usuario', usuario_schema)
 
+/*
+*   Pruebas para verficar que funcione
+*
+*
+  var tonto = new Usuario({
+    username   : '1234',
+    nombre          :   'nombe',
+    apellido           : 'apellido',
+    legajo              : '123',
+    password        :  '1234',
+});
+
+tonto.save();
+
+
+Usuario.getAuthenticated('123', '123', function(err, usuario, motivo) {
+        if (err) throw err;
+
+        // login was successful if we have a user
+        if (usuario) {
+            // handle login success
+            console.log('login success');
+            return;
+        }
+        console.log(motivo);
+    });
+*/
 
   exports.login = function (req, res, next) {
 
     var username = req.body.username;
-
     var password = req.body.password;
 
-    if(username==='admin'){
-       var dto={
-            _id:1,
-            username:'admin',
-            rol:'admin',
-        }
-        res.json(dto);
-    }else{
 
-    Usuario.findOne({ nombre: username}, function (err, usuario) {
-    if (err){ //return handleError(err);
-        res.writeHead(401)
+Usuario.getAuthenticated(username, password, function(err, usuario, motivo) {
+        if (err) throw err;
+        if (usuario) {
+            var usuariodto={
+                _id:usuario.id,
+                username:usuario.nombre,
+                roles:usuario.roles,
+                nombre:usuario.nombre,
+                apellido:usuario.apellido,
+            }
+            res.json(usuariodto);
+        }else{
+            res.writeHead(401)
         res.send();
-    }
-    if(password===usuario.password){
-        var usuariodto={
-            _id:usuario.id,
-            username:usuario.nombre,
-            rol:usuario.rol,
         }
-        res.json(usuariodto);
-    }else{
-        res.writeHead(401)
-        res.send();
-    }
-    })
-    }
-}
+
+    });
+};
--- models/usuario.js
+++ models/usuario.js
@@ -1,12 +1,78 @@
-var Schema = require('mongoose').Schema
+var mongoose = require('mongoose'),
+    Schema = mongoose.Schema,
+    bcrypt = require('bcrypt'),
+    SALT_WORK_FACTOR = 10;
 
 var usuario_schema = new Schema({
-    username   : String,
+    username   : { type: String, required: true,lowercase:true, index: { unique: true } },
     nombre          :   String,
     apellido           : String,
     legajo              : String,
     password        :  String,
-    id_rol                   :  String,
+    roles                   :  [Schema.ObjectId],
 });
 
+usuario_schema.pre('save', function(next) {
+
+    var usuario = this;
+
+// Si no es un password nuevo no hace nada.
+if (!usuario.isModified('password')) return next();
+
+// genera el salt...
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+
+    if (err) return next(err);
+        // hashea el password con el salt
+        bcrypt.hash(usuario.password, salt, function(err, hash) {
+            if (err) return next(err);
+            // override the cleartext password with the hashed one
+            usuario.password = hash;
+            next();
+        });
+    });
+});
+
+usuario_schema.post('save',function(){
+    console.log("guardo");
+});
+
+
+
+
+
+usuario_schema.methods.comparePassword = function(candidatePassword, cb) {
+
+    bcrypt.compare(candidatePassword, this.password, function(err, isMatch) {
+        if (err) return cb(err);
+        cb(null, isMatch);
+    });
+};
+
+usuario_schema.statics.getAuthenticated = function(username, password, cb) {
+    this.findOne({ username: username }, function(err, usuario) {
+        if (err) throw err;
+
+        // make sure the user exists
+        if (!usuario) {
+            return cb(null,null,"No esta");
+        }
+
+
+        // test for a matching password
+        usuario.comparePassword(password, function(err, isMatch) {
+            if (err) console.log("err");
+
+
+            if (isMatch) {
+                return cb(null, usuario);
+            }else{
+                return cb(null,null,"No coincide");
+            }
+        });
+    });
+};
+
+
 module.exports = usuario_schema
+//module.exports = mongoose.model('Usuario', usuario_schema);
