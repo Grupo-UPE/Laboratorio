9a1d7436b7920e79943fe8103682ad6eb7c1355d
Ferticidio <ferticidio@gmail.com>
2014-09-10 20:20:53 -0300

    Se crean los metodos para trabajar sobre el usuario

--- controllers/usuario.js
+++ controllers/usuario.js
@@ -60,3 +60,120 @@ Usuario.getAuthenticated(username, password, function(err, usuario, motivo) {
 
     });
 };
+
+exports.create = function (req, res, next) {
+    //Las verificaciones de los requeridos la hariamos desde angular.... por ahora.
+    var username=req.body.username;
+    var nombre=req.body.nombre;
+    var apellido=req.body.apellido;
+    var email=req.body.email;
+    var legajo=req.body.legajo;
+    var password=req.body.password;
+    var cambiarpass=true;
+    var roles=req.body.roles;
+
+    var usuario=new Usuario({
+        username:username,
+        nombre:nombre,
+        apellido:apellido,
+        email:email,
+        legajo:legajo,
+        password:password,
+        cambiarpass:cambiarpass,
+        roles:roles,
+    });
+    usuario.save(onSaved)
+
+    function onSaved (err) {
+        if (err) {
+            console.log(err)
+            return next(err)
+        }
+        return res.redirect('/usuarios')
+        }
+};
+
+exports.list = function (req, res, next) {
+
+    Usuario.find(gotUsuarios)
+
+  function gotTextos (err, usuarios) {
+    if (err) {
+      console.log(err)
+      return next()
+    }
+    /* Deberiamos utilizar un usuariodto o algo similar.
+    No le veo mucho sentido a enviar toda la informacion del usuario.
+    Me molesta, sobre todo, enviar el password, aunque esta hasheado
+    */
+    return res.json(usuarios);
+  }
+
+}
+
+exports.show = function (req, res, next) {
+  var id = req.params.id
+
+  Texto.findById(id, gotUsuario)
+
+  function gotTexto (err, usuario) {
+    if (err) {
+      console.log(err)
+      return next(err)
+    }
+    var usuariodto={
+                _id:usuario.id,
+                username:usuario.nombre,
+                roles:usuario.roles,
+                nombre:usuario.nombre,
+                apellido:usuario.apellido,
+            }
+    return res.json(usuariodto)
+  }
+};
+
+exports.update = function (req, res, next) {
+
+    var id = req.body._id
+    var username=req.body.username;
+    var nombre=req.body.nombre;
+    var apellido=req.body.apellido;
+    var email=req.body.email;
+    var legajo=req.body.legajo;
+    var password=req.body.password;
+    var cambiarpass=true;
+    var roles=req.body.roles;
+
+
+  if ((username=== '')) {
+    return res.send({'error':'Debe escribir algo'})
+  }
+
+    Usuario.findById(id, gotUsuario)
+
+    function gotUsuario (err, usuario) {
+        if (err) {
+            return next(err)
+        }
+        if (!usuario) {
+            return res.send({'error':'ID invalido'})
+        } else {
+            usuario.username=username;
+            usuario.nombre=nombre;
+            usuario.apellido=apellido;
+            usuario.email=email;
+            usuario.legajo=legajo;
+            usuario.password=password;
+            usuario.roles=roles;
+            texto.save(onSaved)
+        }
+    }
+
+    function onSaved (err) {
+        if (err) {
+            console.log(err)
+            return next(err)
+        }
+        return res.send('')
+        }
+}
