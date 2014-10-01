8d9858447c0e6352b94e9fc2283d28accd76d7cc
Ferticidio <ferticidio@gmail.com>
2014-10-01 18:18:00 -0300

    Se agrega referencia de habilidad en postulante

--- models/postulante.js
+++ models/postulante.js
@@ -15,7 +15,8 @@ var postulante_schema = new Schema({
     disponibilidad: String,
     experiiencia_laboral: [{ empresa: String, antiguedad: String, rol: String}],
     comentario: String,
-    habilidades: [{ tecnologia: String, categoria: String}],
+    habilidades     : [{type: Schema.ObjectId, ref : 'Habilidad'}],
+    habilidades_evaluadas     : [{{type: Schema.ObjectId, ref : 'Habilidad'},Evaluacion:Number}],
 });
 
 module.exports = mongoose.model('Postulante', postulante_schema);
