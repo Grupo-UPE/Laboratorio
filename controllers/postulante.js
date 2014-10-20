8867e846f0a21ec3565bb5441e28b75ddf310551
Ferticidio <ferticidio@gmail.com>
2014-10-20 17:33:24 -0300

    Busqueda por lista de habilidades mejorada

--- controllers/postulante.js
+++ controllers/postulante.js
@@ -295,10 +295,11 @@ exports.upload = function (req, res, next) {
     res.send(JSON.stringify(postulante));
 }
 exports.listarPorHabilidades = function (req, res, next) {
-    console.log(req.body.habilidades);
     var habs= [];
+    var habId=[];
     for(var id in req.body.habilidades){
-      habs.push({habilidades: req.body.habilidades[id]["_id"]});
+        habId.push(req.body.habilidades[id]["_id"]);
+        habs.push({habilidades: req.body.habilidades[id]["_id"]});
     }
 
     console.log(habs);
@@ -311,7 +312,25 @@ exports.listarPorHabilidades = function (req, res, next) {
             return next()
         }
 
-        console.log(postulantes);
+        var postulantesPuntuados=[]; //{postulante,coincidencias,puntaje}
+        var habilidadesPunteadas=habId.reverse();
+        var puntos=0;
+        var listaHabilidades=[]
+
+        for (var i = 0; i < postulantes.length; i++) {//Cada postulante
+            for(var j=0; j<habilidadesPunteadas.length; j++){ //Cada habilidad recibida
+                for(var t=0; t<postulantes[i].habilidades.length; t++){ //Cada habilidad individual de cada postulante
+                    if(postulantes[i].habilidades[t]._id==habilidadesPunteadas[j]){ //Si coinciden lo meto dentro del lsitado
+                        puntos=puntos+(j+1);
+                        listaHabilidades.push(postulantes[i].habilidades[t]);
+                    }
+                }
+            }
+            postulantesPuntuados.push({postulante:postulantes[i],coincidencia:listaHabilidades,puntaje:puntos}); //postulante, array de coincidencias, puntaje
+            puntos=0;
+            listaHabilidades=[];
+        };
+        console.log( postulantesPuntuados.sort(function(a, b){return b.puntaje-a.puntaje}));
         //return res.json(postulante);
     }
 
