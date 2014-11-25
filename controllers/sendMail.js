43711a03bd921b589dbd5bf5aa155fd79cf48c15
Ferticidio <ferticidio@gmail.com>
2014-11-25 12:02:02 -0300

    Send mail funional. Ccodigo de Carlos Le cambie las opciones ya que intentaba enviar html que todavia no tenemos

--- controllers/sendMail.js
+++ controllers/sendMail.js
@@ -2,37 +2,44 @@ var nodemailer = require('nodemailer');
 gapi = require('../lib/gapi');
 
 exports.enviomail = function(req, res, next) {
-var smtpTransport = nodemailer.createTransport("SMTP",{
-   service: "Gmail",  // sets automatically host, port and connection security settings
-auth: {
-	XOAuth2: {        
-			user: "desarrollotest80@gmail.com",         
-			clientId: gapi.a.clientId_,        
-			clientSecret: gapi.a.clientSecret_,       
-			//refreshToken: '1/CpHTWNfFhG56KoLXcYTfu3KKevknDovZGpPtOPXvW0EMEudVrK5jSpoR30zcRFq6',//req.session.tokens.token,
-			refreshToken: gapi.a.credentials.id_token,         
-			accessToken: gapi.a.credentials.access_token             
-		}    
-	}  
-});
-    
-	var mailOptions={
-		to : req.body.to,
-		subject : req.body.subject,
-		text : req.body.content,
-		generateTextFromHTML: true,
-  		html: "<b>Hello world</b>"
-	}
-	console.log(mailOptions);
-	
-	smtpTransport.sendMail(mailOptions, function(error, response){
-		if(error){
-		console.log(error);
-		res.end("error");
-		}else{
-		console.log("Message sent: " + response.message);
-		res.end("sent");
-		}
-	});
 
-};
+
+    gapi.b.people.get({ userId: 'me', auth: gapi.a }, function(err, profile) {
+    if (err) {
+        console.log('An error occured', err);
+        return;
+    }
+    //console.log(profile); //El usuario se logueo, en teoria.
+    var email=profile.emails;
+
+    var smtpTransport = nodemailer.createTransport("SMTP",{
+       service: "Gmail",
+    auth: {
+        XOAuth2: {
+                user: email[0].value,
+                clientId: gapi.a.clientId_,
+                clientSecret: gapi.a.clientSecret_,
+                refreshToken: gapi.a.credentials.id_token,
+                accessToken: gapi.a.credentials.access_token
+            }
+        }
+    });
+        var mailOptions={
+            from: email[0].value,
+            to : req.body.to,
+            subject : req.body.subject,
+            text : req.body.content,
+        }
+        console.log(mailOptions);
+
+        smtpTransport.sendMail(mailOptions, function(error, response){
+            if(error){
+                console.log(error);
+                res.end("error");
+            }else{
+                console.log("Message sent: " + response.message);
+                res.end("sent");
+            }
+        });
+     });
+}
